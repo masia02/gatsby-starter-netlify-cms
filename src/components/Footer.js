1970f90d8fbd7efe15b755487e6ffe9c0fbac0a1
Masamichi Takano <masia02@gmail.com>
2020-07-07 21:47:41 +0900

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,14 +74,14 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://facebook.com/cipherweb">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/cipherwebllc">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -89,14 +89,14 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://instagram.com/cipherweb">
                   <img
                     src={instagram}
                     alt="Instagram"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
+                <a title="vimeo" href="https://vimeo.com/masia02">
                   <img
                     src={vimeo}
                     alt="Vimeo"
